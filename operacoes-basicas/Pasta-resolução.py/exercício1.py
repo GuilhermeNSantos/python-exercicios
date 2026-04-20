099886e332ad6de4e6d4b95d5d5f9e2c60b42eea
Guilherme Nascimento Santos <guilhermenascimentosantos35@gmail.com>
2026-04-20 19:04:48 -0300

    Primeiro código - área do retângulo

--- operacoes-basicas/Pasta-resolução.py/exercício1.py
+++ operacoes-basicas/Pasta-resolução.py/exercício1.py
@@ -1,1 +1,7 @@
+#Exercício - 1. Faça um algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno?.
 
+altura = float(input("Informe qual a altura do terreno em metros : "))
+largura = float(input("Informe qual a largura do terreno em metros :"))
+
+area = (altura * largura)
+print(f"O terreno possui {area} m²")
